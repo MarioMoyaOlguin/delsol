94abb4530823c0f55f042838e2e9cdcd5a37efee
Mario Moya <mariomoyaolguin@gmail.com>
2022-12-28 22:11:30 -0600

    Se agregaron estilos responsivos a la pantalla home y se agrego interactividad a la pantalla de pregunta tipo calificacion, se puede seleccionar calificacion y que se guarda en un arreglo que la marca como activa

--- src/app/calificacion/calificacion.component.ts
+++ src/app/calificacion/calificacion.component.ts
@@ -10,6 +10,24 @@ export class CalificacionComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.scoreArr)
+  }
+
+  scoreArr = new Array(10).fill({active: false});
+
+
+  activate = (index:number) => {
+    this.scoreArr = [];
+
+    for (let i = 0; i < 10; i++) {
+      if(index === i) {
+        this.scoreArr.push({index: i, active: true});
+
+      } else {
+        this.scoreArr.push({index: i, active: false});
+      }
+    }
+    console.log(this.scoreArr)
   }
 
 }
